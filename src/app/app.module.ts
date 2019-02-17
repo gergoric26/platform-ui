5f91afd09a161a3a2669a9e5c859849c5c73aeb5
YOUR NAME <gergoric_dean@yahoo.com>
2019-02-16 22:14:02 -0500

    some code

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    DragDropModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+
+  public selectedProviders = [];
+  public unselectedProviders = [
+    {
+      id: '1',
+      name: 'John',
+      address: '123 Greenway Blvd',
+      phone: '8991234321'
+    },
+    {
+      id: '2',
+      name: 'Mary',
+      address: '443 Windwhisper Road',
+      phone: '2233211903'
+    },
+    {
+      id: '3',
+      name: 'Jason',
+      address: '9992 Pumpkin Hollow',
+      phone: '4343219384'
+    }
+  ];
+
+  // drop(event: CdkDragDrop<string[]>) {
+  //   if (this.selectedProviders !== this.unselectedProviders) {
+  //     transferArrayItem(this.unselectedProviders, this.selectedProviders, event.previousIndex, event.currentIndex);
+  //   } else {
+  //     moveItemInArray(this.unselectedProviders, event.previousIndex, event.currentIndex);
+  //   }
+  // }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
+
+  constructor() {}
+
+
+  ngOnInit() {}
+
+}
